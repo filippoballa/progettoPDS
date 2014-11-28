f4e7d929613e572c91efb743ddbcbacac5137ff2
PazzoDave89 <davide_belfiore@live.it>
2014-11-28 21:53:48 +0100

    New Client LoginForm
    Modifica grafica del login lato client e abbozzo della classe
    SocketConnection lato client

---
+++ ProgettoPDS_CLIENT/ProgettoPDS_CLIENT/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgettoPDS_CLIENT
+{
+    public partial class MainForm : Form
+    {
+        private User user;
+
+        public MainForm( User aux)
+        {
+            InitializeComponent();
+            this.user = aux;
+            this.Text += " - " + aux.Name;
+        }
+    }
+}
---
+++ ProgettoPDS_CLIENT/ProgettoPDS_CLIENT/SocketConnection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ProgettoPDS_CLIENT
+{
+    class SocketConnection
+    { 
+        private Socket sock;
+        private IPEndPoint remoteEP, localEP;
+        private int localPort, remotePort;
+        private static ManualResetEvent connectDone = new ManualResetEvent(false);
+
+        // Costruttore Client
+        public SocketConnection(string addr, int port ) 
+        {            
+            this.GetLocalIP();
+            this.remoteEP = new IPEndPoint(IPAddress.Parse(addr), Convert.ToInt16(port) );
+            this.remotePort = port;
+
+            try {
+                this.sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            }
+            catch (Exception ecc) {
+                MessageBox.Show(ecc.ToString(), "ANOMALY", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        // Ottiene configurazione Locale dell'Host
+        private void GetLocalIP()
+        {
+            IPHostEntry host = Dns.GetHostEntry( Dns.GetHostName() );
+            this.localPort = 5000;
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localEP = new IPEndPoint(ip, this.localPort);
+                    break;
+                }
+            }
+
+        }
+
+        // Esegue una connessione al Server
+        public void StartClientConnection() 
+        {
+            try {
+                this.sock.BeginConnect(this.remoteEP, new AsyncCallback(this.ConnectCallback), sock);
+                connectDone.WaitOne();
+
+
+            }
+            catch (Exception ecc) {
+                MessageBox.Show(ecc.ToString(), "ANOMALY", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult ar) 
+        {
+            try {
+
+                Socket client = (Socket)ar.AsyncState;
+                client.EndConnect(ar);
+                connectDone.Set();
+            }
+            catch (Exception ecc) {
+                MessageBox.Show(ecc.ToString(), "ANOMALY", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connectDone.Set();
+            }
+        }
+
+    }
+}
