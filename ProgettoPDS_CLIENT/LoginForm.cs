31dedb0d4ced80a3139a1fe985531136f6794b16
PazzoDave89 <davide_belfiore@live.it>
2014-12-01 15:34:29 +0100

    Aggiustamento Cartella PDS_CLIENT

--- ProgettoPDS_CLIENT/LoginForm.cs
+++ ProgettoPDS_CLIENT/LoginForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ProgettoPDS_CLIENT
 {   
@@ -16,16 +17,25 @@ namespace ProgettoPDS_CLIENT
 
         public LoginForm( User aux )
         {
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
             InitializeComponent();
             this.user = aux;
         }
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
-            if ( VerificaCredenziali() == true) 
+            if (VerificaCredenziali() == true)
                 this.Close();
-            else
-                MessageBox.Show("Login Fallito!!", "ERRORE", MessageBoxButtons.OK,MessageBoxIcon.Error);         
+            else {
+                MessageBox.Show("Login Fallito!!", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.UsernameTextBox.Clear();
+                this.PasswordTextBox.Clear();
+                this.UsernameTextBox.Focus();
+            }
         }
 
         private bool VerificaCredenziali()
@@ -40,5 +50,27 @@ namespace ProgettoPDS_CLIENT
             else
                 return false;
         }
+
+        private void chiudiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void registraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.MainPanel.Visible = false;
+            this.NameRegTextBox.Focus();            
+            this.RegistraPanel.Visible = true;
+        }
+
+        private void loginPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.RegistraPanel.Visible = false;
+            this.UsernameTextBox.Focus();
+            this.MainPanel.Visible = true;
+
+        }
+
+        
     }
 }
