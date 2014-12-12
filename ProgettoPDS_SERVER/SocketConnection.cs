00e28769a4db4b7562142355c9a73f70f56ea20b
filippo.balla <filippo.balla@gmail.com>
2014-12-12 13:35:54 +0100

    modifiche socket connect

--- ProgettoPDS_SERVER/SocketConnection.cs
+++ ProgettoPDS_SERVER/SocketConnection.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-//using System.Text.Encoding;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
@@ -11,6 +10,33 @@ using System.Windows.Forms;
 
 namespace ProgettoPDS_SERVER
 {
+    /*PROTOCOLLO AUTENTICAZIONE/REGISTRAZIONE
+     * 
+     * +AUTH_USER(SERVER)
+     * +"USERNAME"(CLIENT)
+     * 
+     * USER PRESENTE:
+     * * +AUTH_PWD(SERVER)
+     * * +"PASSWORD"(CLIENT)
+     * 
+     * USER NON PRESENTE:
+     * * +REG_USER(SERVER)
+     * *
+     * * SE IL CLIENT VUOLE REGISTRARSI:
+     * * * +YES(CLIENT)
+     * * * +REG_PWD(SERVER)
+     * * * "PASSWORD"(CLIENT)
+     * 
+     * * SE IL CLIENT VUOLE REGISTRARSI:
+     * * * +NO(CLIENT)
+     * * * chiudo la connessione
+     * 
+     * +OK(SERVER)
+     * CONNESSIONE CREATA CON SUCCESSO
+     * -ERR(SERVER)
+     * ERRORE CONNESSIONE CHIUSA AUTENTICAZIONE NON RIUSCITA, ANCHE SE RICEVO UN ERR DAL CLIENT
+     * 
+     */
     class SocketConnection
     {
         private const int backlog = 1;
@@ -18,6 +44,14 @@ namespace ProgettoPDS_SERVER
         public static ManualResetEvent allDone = new ManualResetEvent(false);
         private int porta;
         private String myIP;
+
+        private const String AUTH_USER = "+AUTH_USER";
+        private const String AUTH_PWD = "+AUTH_PWD";
+        private const String REG_USER = "+REG_USER";
+        private const String YES = "+YES";
+        private const String REG_PWD = "+REG_PWD";
+        private const String OK = "+OK";
+        private const String ERR = "-ERR";
 
         public SocketConnection(int porta)
         {
@@ -31,17 +65,12 @@ namespace ProgettoPDS_SERVER
         }
         public void StartListening()
         {
-            
             try
             {
                 sock.Listen(backlog);
-
-                    // Set the event to nonsignaled state.
-                    //allDone.Reset();
-                    // Program is suspended while waiting for an incoming connection.
-                    sock.BeginAccept(new AsyncCallback(AcceptCallback), sock);
-                    // Wait until a connection is made before continuing.
-                    //allDone.WaitOne();
+                // Program is suspended while waiting for an incoming connection.
+                sock.BeginAccept(new AsyncCallback(AcceptCallback), sock);
+                // Wait until a connection is made before continuing.
             }
             catch (Exception e)
             {
@@ -51,16 +80,18 @@ namespace ProgettoPDS_SERVER
 
         private void AcceptCallback(IAsyncResult ar)
         {
-            //allDone.Set();
-
             passiv = (Socket)ar.AsyncState;
             passiv = passiv.EndAccept(ar);
 
             MessageBox.Show("Connessione avvenuta inizio autenticazione client("+passiv.RemoteEndPoint.ToString()+")");
             if(ClientAutentication())
-            {}
+            {
+                MessageBox.Show("Autenticazione Client effettuata con successo! Connessione avviata.");
+            }
             else
-            {}
+            {
+                MessageBox.Show("Autenticazione Client non riuscita. Chiusura connessione.","errore",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
         }
 
         private bool ClientAutentication()
@@ -70,57 +101,125 @@ namespace ProgettoPDS_SERVER
 
             //richiesta user e password;
 
-            comando = "+AUTH_USER";
+            comando = AUTH_USER;
             data = Encoding.ASCII.GetBytes(comando);
-            passiv.Send(data);//invio richiesta di autenticazione
 
-            passiv.Receive(data);
-            user = Encoding.ASCII.GetString(data);
+            try {
+                passiv.Send(data);//invio richiesta di autenticazione AUTH_USER
 
-            // controllo che l'user sia già registrato
-            if (ControlloUser(user))
-            {
-                comando = "+AUTH_PWD";
-                data = Encoding.ASCII.GetBytes(comando);
-                passiv.Send(data);//se l'user è registrato gli chiedo la pswrd
+                data = new byte[128];
+                passiv.Receive(data);//ricevo l'USER
+                user = Encoding.ASCII.GetString(data);
 
-                passiv.Receive(data);
-                pwd = Encoding.ASCII.GetString(data);
-
-                if (ControlloPassword(user))
+                // controllo che l'user sia già registrato
+                if (user != ERR)
                 {
-                    //...
-                }
-                else//la password non è corretta
+                    MessageBox.Show(user + " ha richiesto di autenticarsi.");
+
+                    if (ControlloUser(user))
+                    {
+                        comando = AUTH_PWD;
+
+                        data = Encoding.ASCII.GetBytes(comando);
+                        passiv.Send(data);//se l'user è registrato gli chiedo la pswrd AUTH_PWD
+
+                        data = new byte[128];
+                        passiv.Receive(data);//ricevo la password
+                        pwd = Encoding.ASCII.GetString(data);
+
+                        if (pwd != ERR)
+                        {
+                            if (ControlloPassword(user, pwd))
+                            {
+                                comando = OK;
+                                data = Encoding.ASCII.GetBytes(comando);
+                                passiv.Send(data);//invio che l'autenticazione è riuscita
+                            }
+                            else//se la password non è corretta si chiude la connessione
+                            {
+                                comando = ERR;
+                                data = Encoding.ASCII.GetBytes(comando);
+                                passiv.Send(data);//invio che c'è stato un errore
+
+                                SockDisconnect();//e chiudo
+                                return false;
+                            }
+                        }
+                        else//è arrivato un ERR dopo AUTH_PWD
+                        {
+                            SockDisconnect();//e chiudo
+                            return false;
+                        }
+                    }
+                    else//se il client non è presente chiedo di registrarsi
+                    {
+                        comando = REG_USER;
+                        data = Encoding.ASCII.GetBytes(comando);
+
+                        passiv.Send(data);//se l'user è registrato gli chiedo la pswrd
+
+                        data = new byte[128];
+                        passiv.Receive(data);
+                        comando = Encoding.ASCII.GetString(data);
+
+                        if (comando == YES)//se vuole registrarsi chiedo la password
+                        {
+                            comando = REG_PWD;
+                            data = Encoding.ASCII.GetBytes(comando);
+
+                            passiv.Send(data);//richiesta pswrd
+
+                            data = new byte[128];
+                            passiv.Receive(data);//password ricevuta
+                            pwd = Encoding.ASCII.GetString(data);
+
+                            if (!AddNewUser(user, pwd))//inserimento non riuscito
+                            {
+                                comando = ERR;
+                                data = Encoding.ASCII.GetBytes(comando);
+                                passiv.Send(data);//invio che c'è stato un errore
+
+                                SockDisconnect();//e chiudo
+                                return false;
+                            }
+                        }else// se non ricevo YES
+                        {
+                            SockDisconnect();
+                            return false;
+                        }
+
+                    }
+                }else//se ricevo un ERR dopo AUTH_USER
                 {
-                    //...
+                    SockDisconnect();
+                    return false;
                 }
+                
             }
-            else if (user == "+REGISTRATION")//se il client ha richiesto di registrarsi
+            catch (Exception e)
             {
-                comando = "";
-                data = Encoding.ASCII.GetBytes(comando);
-                //..
-            }
-            else//se il client non vuole registrarsi si chiude la connessione
-            { 
-                CloseSocket(); 
+                MessageBox.Show(e.Message);
+                SockDisconnect();//e chiudo
+                return false;
             }
             return true;
         }
 
-        private void CloseSocket()
+        private bool AddNewUser(string user, string pwd)
         {
-            throw new NotImplementedException();
+            //TODO
+            return true;
         }
 
-        private bool ControlloPassword(string user)
+        private bool ControlloPassword(string user, string pwd)
         {
+            //TODO
             return true;
         }
 
         private bool ControlloUser(string user)
         {
+            //TODO
             return true;
         }
 
@@ -137,6 +236,11 @@ namespace ProgettoPDS_SERVER
                 }
             }
             return myIP;
+        }
+
+        public void SockDisconnect() {
+            sock.Shutdown(SocketShutdown.Both);
+            sock.Close();
         }
 
     }
